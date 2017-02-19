4f060926d049daf69962cd629d051ffce2aa932d
mindziukasss <mindziukasss@gmail.com>
2017-02-19 21:56:47 +0200

    Creating controllers

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -1,43 +1,45 @@
 <?php
 class Bootstrap{
-    private $controller;
-    private $action;
-    private $request;
+  private $controller;
+  private $action;
+  private $request;
 
-
-    public function __construct($request){
-      $this->request = $request;
-      if (isset($request['controller'])){
-          $this->controller = 'home';
-      } else{
-          isset($this->request['controller']);
-      }
-      if(isset($request['action'] )){
-          $this->action = 'index';
-      } else {
-          isset($this->request['action']);
-      }
+  public function __construct($request){
+    $this->request = $request;
+    if(@$this->request['controller'] == ""){
+      $this->controller = 'home';
+    } else {
+      $this->controller = $this->request['controller'];
     }
+    if(@$this->request['action'] == ""){
+      $this->action = 'index';
+    } else {
+      $this->action = $this->request['action'];
+    }
+  }
 
-
-    public function createController(){
-      // Chek Class
-      if(class_exists($this->controller)){
-          $parents = class_parents($this->controller);
-          if(in_array("Controller", $parents)){
-              if(method_exists($this->controller, $this->action)){
-                return new $this->controller($this->action, $this->request);
-              }else{
-                echo '<h1>Method does not exist</h1>';
-                return;
-              }
-          }else{
-            echo '<h1>Base controller not found</h1>';
-            return;
-          }
-      } else{
-        echo '<h1>Controller class does not exist</h1>';
+  public function createController(){
+    // Check Class
+    if(class_exists($this->controller)){
+      $parents = class_parents($this->controller);
+      // Check Extend
+      if(in_array("Controller", $parents)){
+        if(method_exists($this->controller, $this->action)){
+          return new $this->controller($this->action, $this->request);
+        } else {
+          // Method Does Not Exist
+          echo '<h1>Method does not exist</h1>';
+          return;
+        }
+      } else {
+        // Base Controller Does Not Exist
+        echo '<h1>Base controller not found</h1>';
         return;
       }
+    } else {
+      // Controller Class Does Not Exist
+      echo '<h1>Controller class does not exist</h1>';
+      return;
     }
+  }
 }
