c7d7560797cae4d3f16903806a68e70cd79c5adc
mindziukasss <mindziukasss@gmail.com>
2017-02-20 18:41:35 +0200

    Added User login & sessions

---
+++ views/users/login.php
@@ -0,0 +1,23 @@
+<div class="container">
+  <div class="row">
+    <div class="center-block" style="width:400px;" >
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">User Login</h3>
+        </div>
+        <div class="panel-body">
+        <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+          <div class="form-group">
+            <label>Email</label>
+            <input type="text" name="email" class="form-control"/>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control"/>
+          </div>
+          <input class="btn btn-primary" name="submit" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
